ae3f6e0cb67afe6be072faf805ebc1e83cc6d7fb
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-01 12:41:33 +0000

    Deploy sunatullo-gafurov/tele-2 to github.com/sunatullo-gafurov/tele-2.git:gh-pages

---
+++ precache-manifest.1b44a72b14a61745615ecf942ed24302.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7142f0ee49749cb9dc4575677c0209c1",
+    "url": "/tele-2/index.html"
+  },
+  {
+    "revision": "d365a67709331fa10e16",
+    "url": "/tele-2/static/css/main.5e734560.chunk.css"
+  },
+  {
+    "revision": "2a8cf8c1943b2477aa5f",
+    "url": "/tele-2/static/js/2.1223c010.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tele-2/static/js/2.1223c010.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d365a67709331fa10e16",
+    "url": "/tele-2/static/js/main.c40f1511.chunk.js"
+  },
+  {
+    "revision": "fbb14b58b4f698c3c359",
+    "url": "/tele-2/static/js/runtime-main.2ad6ba0a.js"
+  }
+]);
